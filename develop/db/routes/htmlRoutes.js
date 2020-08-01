c777640f57548948e5e2072642242c32fd22aaea
Benjamin Turnbull <benorule@outlook.com>
2020-08-01 15:42:20 +0930

    added html routes

---
+++ develop/db/routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+var path = require("path");
+
+module.exports = function(app) {
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../develop/public/notes.html"));
+  });
+
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+  });
+};
