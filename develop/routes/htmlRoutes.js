24e79b266e51675593e40f2c54794a50a597a6fe
Benjamin Turnbull <benorule@outlook.com>
2020-08-05 16:21:18 +0930

    completed html route connection

---
+++ develop/routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+// HTML Routes
+const http = require('http');
+const fs = require('fs');
+var path = require('path');
+
+module.exports = function(app) {
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  // app.get("/reserve", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  // });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
