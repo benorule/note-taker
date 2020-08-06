b0e8111355d2eb5d564c5550e27f98f6d458b412
Benjamin Turnbull <benorule@outlook.com>
2020-08-06 15:26:12 +0930

    created new api route

---
+++ develop/routes/apiRoutes.js
@@ -0,0 +1,19 @@
+var db = require("../db/db.json");
+
+
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        res.json(db);
+    });
+
+    app.post("/api/notes", function (req, res) {
+        db.push(req.body);
+        db.push(db);
+        res.json(true)
+    });
+
+    app.delete("/api/notes/id", function (req, res) {
+        db.push(req.body);
+        res.json(true)
+    });
+};
